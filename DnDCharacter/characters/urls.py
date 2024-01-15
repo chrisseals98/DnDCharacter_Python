80f1c4e9116704d5a0d0dad71b79f905791b0811
chrisseals98 <chrisseals9893@gmail.com>
2024-01-15 10:59:13 -0500

    added admin link

--- DnDCharacter/characters/urls.py
+++ DnDCharacter/characters/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "characters"
 urlpatterns = [
     path("", views.ListView.as_view(), name="list"),
-    path("<int:pk>", views.DetailedView.as_view(), name="detail")
+    path("<int:pk>/", views.DetailedView.as_view(), name="detail")
 ]
